f20c02a0c22f02e179a38c418e1a71b1807c8dcd
Bithoa <47375971+Bithoa@users.noreply.github.com>
2022-10-31 19:02:37 -0700

    mostly done
    mostly done, maybe a few rough edges to iron out, but pretty much good

--- hwutils.py
+++ hwutils.py
@@ -8,10 +8,12 @@ import matplotlib.pyplot as plt
 
 def plot_pca(pca , 
              bigwig_metadata=None,
-             metadata_label_column=None, 
+             metadata_label_column=None,
+             metadata_label_column2=None, 
              alpha=0.5, 
              lw=0, 
-             figsize=(8,8)):
+             figsize=(8,8),
+             label_points=False):
     
     """ 
     Skeleton for plotting PCA and annotating the plot. 
@@ -25,11 +27,19 @@ def plot_pca(pca ,
         labels = [bigwig_metadata.query(
                     "`File accession`==@ file_accession ").loc[:,metadata_label_column].values[0]
                   for file_accession in pca.feature_names_in_]
+        if metadata_label_column2 is not None:
+            labels2 = [bigwig_metadata.query(
+                "`File accession`==@ file_accession ").loc[:,metadata_label_column2].values[0]
+                for file_accession in pca.feature_names_in_]
+            for x in range(len(labels)):
+                labels[x]=labels[x] + ", " + labels2[x]
+
+           
         le = sklearn.preprocessing.LabelEncoder()
         le.fit(labels)
         labels = le.transform(labels)
     else: 
-        labels = None
+        labels = None   
         
     plt.figure(figsize=figsize)
     p = plt.scatter(pca.components_[0],
@@ -40,10 +50,14 @@ def plot_pca(pca ,
     if labels is not None: 
         plt.legend(handles = p.legend_elements()[0], 
                    labels = le.classes_.tolist())
-        plt.legend(handles = p.legend_elements()[0], 
-                   labels = le.classes_.tolist())
-        plt.title('PCA Plot Colored By: '+metadata_label_column)
+        plt.title('PCA Plot Coloured By: '+metadata_label_column)
                    
     plt.xlabel('PC1')
     plt.ylabel('PC2')
+    
+    if label_points:
+        for x in range(len(pca.components_[0])):
+            plt.text(pca.components_[0][x], pca.components_[1][x], str(x))
+
+    plt.show()
 
